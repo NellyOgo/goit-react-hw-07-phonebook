30e40d5317984f044dc8e1d56d07e0e2b8a1a9f8
NellyOgo <nellyogorodnik@gmail.com>
2023-09-25 22:39:57 +0300

    try Nell

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import { ContactForm } from './Form/Form';
+import { Application, FormTitle } from './App.styled';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+
 export const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Application>
+      <FormTitle>Phonebook</FormTitle>
+      <ContactForm></ContactForm>
+      <hr />
+      <FormTitle>Contacts</FormTitle>
+      <Filter />
+      <ContactList />
+    </Application>
   );
 };
